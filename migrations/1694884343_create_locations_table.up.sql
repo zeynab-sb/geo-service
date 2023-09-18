2c9e3c9a665fa945391286b14a19600743671098
Zeynabsadat Sobhani <zeynab.sobhani6294@gmail.com>
2023-09-18 19:30:59 +0330

    add uniqeue key

--- migrations/1694884343_create_locations_table.up.sql
+++ migrations/1694884343_create_locations_table.up.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS locations (
     mystery_value INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_location UNIQUE (ip_address,country_code,country,city,latitude,longitude,mystery_value),
     PRIMARY KEY(id)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
